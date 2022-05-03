00723096c2d07ed228781468018aa26ef103b3fe
Simachew Tibebu <simachew.tibebu@gmail.com>
2022-05-03 21:25:49 +0300

    create codepipline and ecs custer with one service using a module from terraform registry

--- main.tf
+++ main.tf
@@ -1,19 +1,15 @@
 terraform {
-    # backend "s3" {
-    #   bucket = "terraform-state"
-    #   key    = "iac/terraform.tfstate"
-    #   region = "eu-north-1"
-    # }
+  # backend "s3" {
+  #   bucket = "terraform-state"
+  #   key    = "iac/terraform.tfstate"
+  #   region = "eu-north-1"
+  # }
 }
 
 
 provider "aws" {
   profile = var.profile
   region  = "eu-north-1"
-}
-
-resource "aws_codebu" "name" {
-  
 }
 
 # resource "aws_ecs_service" "equipments_service" {
@@ -29,20 +25,25 @@ module "ecs-codepipeline" {
   source  = "cloudposse/ecs-codepipeline/aws"
   version = "0.28.5"
   # insert the 21 required variables here
-  branch = "master"
-  delimiter = "-"
-  ecs_cluster_name = var.ecs_cluster_name
-  enabled = true
-  environment = terraform.workspace
-  id_length_limit = 12
-  image_repo_name = "codepipeline-ecs-image-${terraform.workspace}"
-  label_key_case = "upper"
-  lable_order = ["environment", "name"]
-  lable_value_case = "upper"
-  name = var.app_name
-  namespace = var.namespace
+  branch              = "master"
+  delimiter           = "-"
+  ecs_cluster_name    = var.ecs_cluster_name
+  enabled             = true
+  environment         = terraform.workspace
+  id_length_limit     = 12
+  image_repo_name     = "codepipeline-ecs-image-${terraform.workspace}"
+  label_key_case      = "upper"
+  label_order         = ["environment", "name"]
+  label_value_case    = "upper"
+  name                = var.app_name
+  namespace           = var.namespace
   regex_replace_chars = "/[^a-zA-Z0-9-]/"
-  region = var.region
-    repo_name = "codepipeline-ecs-image-${terraform.workspace}"
-
+  region              = var.region
+  repo_name           = "codepipeline-ecs-image-${terraform.workspace}"
+  repo_owner          = var.repo_owner
+  # secondary_artifact_bucket_id = 
+  # secondary_artifact_identifier =
+  service_name = "${var.app_name}-${terraform.workspace}"
+  stage        = terraform.workspace
+  tenant       = var.tenant
 }
--- variable.tf
+++ variable.tf
@@ -3,31 +3,36 @@ variable "profile" {
 }
 
 variable "ecs_cluster_name" {
-  type = string
+  type        = string
   description = "Name of the ECS cluster"
 }
 
 variable "app_name" {
-  type = string
+  type        = string
   description = "Name of the application"
 }
 
 variable "namespace" {
-  type = string
+  type        = string
   description = "Name of the namespace"
 }
 
 variable "region" {
-    type = string
-    description = "The AWS region to use"
+  type        = string
+  description = "The AWS region to use"
 }
 
 variable "repo_name" {
-    type = string
-    description = "The name of the GitHub repository"
+  type        = string
+  description = "The name of the GitHub repository"
 }
 
 variable "repo_owner" {
-    type = string
-    description = "The name of the Github repository owner. Orginisation or username"
+  type        = string
+  description = "The name of the Github repository owner. Orginisation or username"
+}
+
+variable "tenant" {
+  type        = string
+  description = "The customer identifier that indicates who this instance is for"
 }
